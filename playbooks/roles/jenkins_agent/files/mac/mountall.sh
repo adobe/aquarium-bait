5aa472d7ea9856f92dc71abee77f3a59e24e0083
Sergei Parshev <parshev@adobe.com>
2021-03-29 16:23:19 -0700

    Improved recovery mode enter and added vmtools for vmware

--- playbooks/roles/jenkins_agent/files/mac/mountall.sh
+++ playbooks/roles/jenkins_agent/files/mac/mountall.sh
@@ -1,13 +1,15 @@
-#!/bin/sh -e
+#!/bin/sh
 # Mounts all the available volumes on external disks
 # It takes some time for MacOS to fill the disks list, so repeating it
 
-for i in $(seq 1 10); do
+# 210 sec to mount the external disks
+for i in $(seq 1 20); do
     disks=$(diskutil list | grep external | cut -d ' ' -f 1)
     echo "Located disks: $disks"
     for disk in $disks; do
         echo "Mounting: $disk..."
         diskutil list "$disk"
+        # Mount could fail if the disk is not healthy
         diskutil mountDisk "$disk"
         mount | grep "^$disk"
     done
---
+++ playbooks/roles/vmtools/README.md
@@ -0,0 +1,7 @@
+# VM Tools role
+
+This role installs VM tools to control VM properly
+
+## Tasks
+
+* Install VMWare tools
